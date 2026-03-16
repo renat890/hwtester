f145e7f457d7b812b194dcdc4ed2c01fd8db78d8
Renat Sedlovsky <rensedlovskij911@gmail.com>
2026-03-16 17:28:30 +0500

    refactor: в отдельный блок

---
+++ internal/tests/utils.go
@@ -0,0 +1,9 @@
+package tests
+
+
+
+func isPassValue(expected, actual int, threshold float64) bool {
+	minMb := float64(expected) * (1 - threshold)
+	maxMb := float64(expected) * (1 + threshold)
+	return float64(actual) >= minMb && float64(actual) <= maxMb
+}
