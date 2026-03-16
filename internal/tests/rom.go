6f5d3927abfb8e05c3c091d6a9b0e08345497c71
Renat Sedlovsky <rensedlovskij911@gmail.com>
2026-03-16 17:28:44 +0500

    feat: тест ПЗУ

---
+++ internal/tests/rom.go
@@ -0,0 +1,73 @@
+package tests
+
+import (
+	"context"
+	"factorytest/internal/config"
+	"factorytest/internal/hw"
+	"fmt"
+	"time"
+)
+
+type DiskInfo struct {
+	Name string
+	VolumeMB int
+}
+
+type DisksGetter interface {
+	Info() ([]DiskInfo, error)
+}
+
+type ROM struct {
+	name string
+	dg DisksGetter
+	conf config.ROM
+}
+
+func NewTestRom(dg DisksGetter, conf config.ROM) *ROM {
+	return &ROM{
+		name: "Тест ПЗУ",
+		dg: dg,
+		conf: conf,
+	}
+}
+
+func (r *ROM) Run(ctx context.Context) (result hw.TestResult) {
+	const threshold float64 = 0.0
+	start := time.Now()
+	result = hw.TestResult{Name: r.name}
+
+	defer func() { result.Duration = time.Since(start) }()
+
+	actualDisks, err := r.dg.Info()
+	if err != nil {
+		result.Status = hw.Error
+		result.Details = "Ошибка получения информации о дисках"
+		return result
+	}
+
+	if nums := len(actualDisks); nums != r.conf.Nums {
+		result.Status = hw.Fail
+		result.Details = fmt.Sprintf("В системе установлено %d дисков, а должно быть %d дисков", nums, r.conf.Nums )
+		return result
+	}
+
+	for _, disk := range actualDisks {
+		if isPassValue(r.conf.ValueMBEach, disk.VolumeMB, threshold) {
+			continue
+		}
+		result.Status = hw.Fail
+		result.Details = fmt.Sprintf("Для диска %s объем %d, а должно быть %d", disk.Name, disk.VolumeMB, r.conf.ValueMBEach )
+		return result
+	}
+
+	result.Metrics = map[string]any{}
+	result.Metrics["nums_disks"] = len(actualDisks)
+	result.Metrics["volume_each_disk"] = r.conf.ValueMBEach
+	result.Status = hw.Pass
+	return result
+}
+
+
+func (r *ROM) Name() string {
+	return r.name
+}
---
+++ internal/tests/rom_test.go
@@ -0,0 +1,73 @@
+package tests
+
+import (
+	"context"
+	"errors"
+	"factorytest/internal/config"
+	"factorytest/internal/hw"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// ======== мок ==========
+
+type mockRom struct {
+	disks []DiskInfo
+	err error
+}
+
+func (m *mockRom) Info() ([]DiskInfo, error) {
+	return m.disks, m.err
+}
+
+// ======== тест ==========
+
+var conf = config.ROM{
+	Nums: 4,
+	ValueMBEach: 4096,
+}
+
+func TestRom(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	testCases := []struct {
+		desc	string
+		disks []DiskInfo
+		expected hw.Status
+	}{
+		{
+			desc: "4 диска, объемы совпадают PASS",
+			disks: []DiskInfo{{Name: "/dev/sda", VolumeMB: 4096}, {Name: "/dev/sdb", VolumeMB: 4096}, {Name: "/dev/sdc", VolumeMB: 4096}, {Name: "/dev/sde", VolumeMB: 4096}},
+			expected: hw.Pass,
+		},
+		{
+			desc: "3 диска вместо 4 FAIL",
+			disks: []DiskInfo{{Name: "/dev/sda", VolumeMB: 4096}, {Name: "/dev/sdb", VolumeMB: 4096}, {Name: "/dev/sdc", VolumeMB: 4096}},
+			expected: hw.Fail,
+		},
+		{
+			desc: "4 диска, один отличен по объему FAIL",
+			disks: []DiskInfo{{Name: "/dev/sda", VolumeMB: 4096}, {Name: "/dev/sdb", VolumeMB: 4096}, {Name: "/dev/sdc", VolumeMB: 4096}, {Name: "/dev/sde", VolumeMB: 8192}},
+			expected: hw.Fail,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			testROM := NewTestRom(&mockRom{disks: tC.disks} ,conf)
+			actual := testROM.Run(ctx)
+			assert.Equal(t, tC.expected, actual.Status)
+			assert.NotZero(t, actual.Duration) 
+		})
+	}
+}
+
+func TestRomError(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	testROM := NewTestRom(&mockRom{err: errors.New("hardware error")} ,conf)
+	actual := testROM.Run(ctx)
+	assert.Equal(t, hw.Error, actual.Status)
+}
