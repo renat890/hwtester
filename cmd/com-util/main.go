f381a5ba901f962abab6c638c4d95d9fc1004de5
Renat Sedlovsky <rensedlovskij911@gmail.com>
2026-05-04 14:51:16 +0500

    feat: улучшил утилиту

--- cmd/com-util/main.go
+++ cmd/com-util/main.go
@@ -2,28 +2,73 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"os"
 	"strings"
 	"sync"
 	"time"
 
 	"go.bug.st/serial"
+	"gopkg.in/yaml.v3"
 )
 
 const msg = "test com this big message and very big message abracodabra stop"
 
-func main() {
-	ports, err := serial.GetPortsList()
+type Config struct {
+	Ports    []string      `yaml:"ports"`
+	Duration time.Duration `yaml:"duration"`
+}
+
+func mustConf(path string) *Config {
+	fileConf, err := os.Open(path)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Не удалось открыть файл по пути %s %v", path, err)
 	}
-	for _, port := range ports {
-		log.Printf("Найден порт: %v\n", port)
+	var conf Config
+	err = yaml.NewDecoder(fileConf).Decode(conf)
+	if err != nil {
+		log.Fatalf("Не удалось загрузить конфигурацию %s", err)
 	}
 
+	log.Println(conf)
+	return &conf
+}
+
+func main() {
+	log.Println("Добро пожаловать в утилиту тестирования COM-портов!")
+	// log.Println("Сканирование доступных портов в системе:")
+	// ports, err := serial.GetPortsList()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, port := range ports {
+	// 	log.Printf("Найден порт: %v\n", port)
+	// }
+
+	conf := mustConf()
+
+	var portOne, portTwo, path string
+	flag.StringVar(&portOne, "first", "/dev/ttyS0", "first com port for testing")
+	flag.StringVar(&portTwo, "second", "/dev/ttyS1", "second com port for testing")
+	flag.StringVar(&path, "path", "", "path to config.yml with settings")
+
+	flag.Parse()
+	// log.Println("Валидация заданных портов для тестирования:")
+	// if !slices.Contains(ports, portOne) {
+	// 	log.Fatalf("❌ COM-порт %s отсутствует в системе", portOne)
+	// }
+	// if !slices.Contains(ports, portTwo) {
+	// 	log.Fatalf("❌ COM-порт %s отсутствует в системе", portTwo)
+	// }
+	// log.Println("✅ Порты валидны. Запуск теста")
+	// pairs := map[string]string{
+	// 	ports[0]: ports[1],
+	// 	ports[1]: ports[0],
+	// }
 	pairs := map[string]string{
-		ports[0]: ports[1],
-		ports[1]: ports[0],
+		portOne: portTwo,
+		portTwo: portOne,
 	}
 
 	var wg sync.WaitGroup
@@ -35,23 +80,23 @@ func main() {
 
 	for i := range testsAttempt {
 		tests = true
-		ctx, cancel := context.WithTimeout(context.Background(), 2 * time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 
 		log.Printf("%d попытка прохождения теста с COM-портами\n", i+1)
 		for rPort, wPort := range pairs {
 			log.Printf("Тестирование пары rPort %s, wPort %s\n", rPort, wPort)
 			wg.Add(2)
-			go func(r string)  {
+			go func(r string) {
 				defer wg.Done()
 				portRead(ctx, r, result)
 			}(rPort)
 			go func(w string) {
 				defer wg.Done()
 				portWrite(ctx, w)
-			}(wPort) 
+			}(wPort)
 			wg.Wait()
 
-			res, ok := <- result
+			res, ok := <-result
 			if !ok || !res {
 				tests = false
 			}
@@ -60,16 +105,16 @@ func main() {
 		if tests {
 			break
 		} else {
-			log.Println("Неудачная попытка прохождения теста. Перезапускаю тест.")
+			log.Println("❌ Неудачная попытка прохождения теста. Перезапускаю тест.")
 		}
 	}
 
 	if tests {
-		log.Println("Тест пройден")
+		log.Println("✅ Тест пройден")
 	} else {
-		log.Println("Тест не пройден")
+		log.Println("❌ Тест не пройден")
 	}
-	
+
 }
 
 func portRead(ctx context.Context, name string, ch chan bool) {
@@ -83,7 +128,7 @@ func portRead(ctx context.Context, name string, ch chan bool) {
 	}
 	defer port.Close()
 
-	if err := port.SetReadTimeout(500 * time.Millisecond); err !=  nil {
+	if err := port.SetReadTimeout(500 * time.Millisecond); err != nil {
 		log.Println(err)
 		ch <- false
 		return
@@ -97,12 +142,12 @@ func portRead(ctx context.Context, name string, ch chan bool) {
 
 	var final strings.Builder
 	buf := make([]byte, 8)
-	numMsg := 1 
+	numMsg := 1
 	log.Printf("Запущен порт читатель rPort %s\n", name)
-	reader:
+reader:
 	for {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			ch <- false
 			return
 		default:
@@ -122,7 +167,7 @@ func portRead(ctx context.Context, name string, ch chan bool) {
 		}
 	}
 	if msg == final.String() {
-		ch <- true	
+		ch <- true
 	} else {
 		ch <- false
 	}
